df8c9c0f490d56942efe66db5f384214a3f26eae
TankredO <tankred_ott@hotmail.de>
2019-09-17 17:51:20 +0200

    Project

--- ginjinn/core/project.py
+++ ginjinn/core/project.py
@@ -1,4 +1,5 @@
-import importlib_resources
+import importlib_resources as resources
+from ginjinn import data_files
 from pathlib import Path
 
 from ginjinn import config
@@ -6,3 +7,15 @@ from ginjinn import config
 class Project:
     def __init__(self):
         pass
+
+    @staticmethod
+    def create_config_template(fpath):
+        '''
+        Create a configuration template file at fpath.
+
+        Keyword arguments:
+        fpath -- path to new config file
+        '''
+        template = resources.read_text(data_files, 'config_template.yaml')
+        with open(fpath, 'w') as f:
+            f.write(template)
